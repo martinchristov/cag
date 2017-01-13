0e86e063e65c97b1bfc64985f9b6e630a14c0fa7
Martin Christov <martin.christov@gmail.com>
2017-01-13 12:03:02 +0200

    Class (on /booking/passenger)

--- app/scripts/passenger-dropdown-dre.js
+++ app/scripts/passenger-dropdown-dre.js
@@ -2,26 +2,7 @@
 	'use strict';
 	angular.module('cag').directive('passengerDropdown',function($state){
 			return {
-				template:[
-				'<div class="passenger-dropdown">',
-					'<div ng-click="showDropdown=true">',
-						'<span ng-hide="selected"><span ng-if="index>1">add passenger {{index}} info</span><span ng-if="index==1">add lead passenger</span></span>',
-						'<span ng-show="selected">{{selected.name}}</span>',
-						'<ng-include src="\'images/pointer.svg\'"></ng-include>',
-					'</div>',
-					'<div class="dropdown" ng-show="showDropdown">',
-						'<ul>',
-							'<li ng-click="add()">+ new passenger</li>',
-							'<li ng-repeat="contact in existing">',
-							'<div ng-click="select(contact)">',
-							'{{contact.name}}',
-							'</div>',
-							'<div class="edit" ui-sref=".edit({id:contact.id})"><ng-include src="\'images/edit.svg\'"></ng-include>edit</div>',
-							'</li>',
-						'</ul>',
-					'</div>',
-				'</div>'
-				].join(''),
+				templateUrl:'partials/passenger-dropdown.html',
 				replace:true,
 				restrict:'E',
 				scope:{
